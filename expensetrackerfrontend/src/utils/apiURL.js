c359a5c702e235a357c4707967ff08c267416050
Nirmal J13 <nirmalnj2003@gmail.com>
2023-09-23 20:06:56 +0530

    apiurl change

--- expensetrackerfrontend/src/utils/apiURL.js
+++ expensetrackerfrontend/src/utils/apiURL.js
@@ -1,3 +1,3 @@
-export const API_USER_URL = 'http://localhost:9000/api/v1/users';
-export const API_ACCOUNT_URL = 'http://localhost:9000/api/v1/accounts';
-export const API_TRANSACTION_URL = 'http://localhost:9000/api/v1/transactions';
+export const API_USER_URL = 'https://expense-tracker-vercel-frontend.vercel.app/api/v1/users';
+export const API_ACCOUNT_URL = 'https://expense-tracker-vercel-frontend.vercel.app/api/v1/accounts';
+export const API_TRANSACTION_URL = 'https://expense-tracker-vercel-frontend.vercel.app/api/v1/transactions';
